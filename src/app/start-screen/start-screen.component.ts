0e8017308586863c0896625a9aea2e2cfaaacf13
atnbn <86778371+atnbn@users.noreply.github.com>
2022-08-05 14:20:17 +0200

    add new animations to startscreen

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -6,11 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent implements OnInit {
-  
+  arrow:boolean;
   constructor() { }
   
 
   ngOnInit(): void {
   }
-  
+  navigatetoSection(){
+    window.location.href = '#about';
+  }
+
+
+  startAnimation(){
+    this.arrow = true;
+    console.log( 'start animation',this.arrow);
+  }
+
+  stopAnimation(){
+    this.arrow = false;
+    console.log( 'end animation',this.arrow);
+  }
+
 }
+
