996d79eb796f2638aa29c5e96eb3f90b247748cf
atnbn <86778371+atnbn@users.noreply.github.com>
2022-08-24 11:17:37 +0200

    new path new component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { SelectControlValueAccessor } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { GreetingComponent } from './greeting/greeting.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { SuccesComponent } from './succes/succes.component';
 
 const routes: Routes = [
   {path: '', component: StartScreenComponent},
-  {path: 'imprint', component: ImprintComponent}
-
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'success', component: SuccesComponent}
 ];
 
 @NgModule({
